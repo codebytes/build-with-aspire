869cd07750d867e427c6425313fa47609f48d299
Chris Ayers <clayers@gmail.com>
2024-07-26 14:58:42 -0400

    Extracted the api to a separate project from the razor component

--- src/BuildWithAspire.Web/Program.cs
+++ src/BuildWithAspire.Web/Program.cs
@@ -1,3 +1,4 @@
+using BuildWithAspire.Web;
 using BuildWithAspire.Web.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddHttpClient<WeatherApiClient>(client =>
+{
+    // This URL uses "https+http://" to indicate HTTPS is preferred over HTTP.
+    // Learn more about service discovery scheme resolution at https://aka.ms/dotnet/sdschemes.
+    client.BaseAddress = new("http://localhost:5020");
+});
 
 var app = builder.Build();
 
